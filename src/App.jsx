4d3badb7a9b38159ce6002c68ea2e168ff36b879
randomnamejusttogetby <gytis.pranskunas@stud.techin.lt>
2025-03-26 18:29:36 +0200

    start working on getting nearby elements. select each place with clikc on map

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import './App.css';
-import Navbar from './components/Header';
 import Map from './components/Map';
 
 function App() {
   return (
     <div className="App">
-        <Navbar/>
         <Map/>
-
     </div>
   );
 }
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,14 +2,16 @@ import React, { useRef, useEffect, useState } from "react";
 import * as maptilersdk from "@maptiler/sdk";
 import "@maptiler/sdk/dist/maptiler-sdk.css";
 import "./Map.css";
+import haversine from "haversine";
 
 import axios from "axios";
 
 export default function Map() {
     const [places, setPlaces] = useState([]);
+    const [nearby, setNearby] = useState([]);
     const mapContainer = useRef(null);
     const map = useRef(null);
-    const kaunas = { lat: 56, lng: 24 };
+    const lithuania = { lat: 56, lng: 24 };
     const zoom = 6;
     maptilersdk.config.apiKey = "wTdfyLcWw7rfqvFRNgjH";
 
@@ -23,10 +25,18 @@ export default function Map() {
         };
 
         fetchPlaces();
-
     }, []);
 
-    const markPlace = (lng, lat) => new maptilersdk.Marker({color: "#FF0000"}).setLngLat([lng, lat]).addTo(map.current);
+    const markPlace = (place) => {
+        const { name, address, rating, longitude: lng, latitude: lat } = place;
+
+        const popupContent = `<p>${name}</p><p>${address}</p><p>${rating}</p>`;
+
+        const mark = new maptilersdk.Marker({ color: "#FF0000" })
+            .setLngLat([lng, lat])
+            .setPopup(new maptilersdk.Popup().setHTML(popupContent))
+            .addTo(map.current);
+    };
 
     useEffect(() => {
         if (map.current) return; // stops map from intializing more than once
@@ -34,16 +44,25 @@ export default function Map() {
         map.current = new maptilersdk.Map({
             container: mapContainer.current,
             style: maptilersdk.MapStyle.STREETS,
-            center: [kaunas.lng, kaunas.lat],
+            center: [lithuania.lng, lithuania.lat],
             zoom: zoom,
         });
+    }, [lithuania.lng, lithuania.lat, zoom]);
 
-
-    }, [kaunas.lng, kaunas.lat, zoom]);
-
-
-    if (places.length)
-    places.forEach((p) => markPlace(p.longitude, p.latitude))
+    if (places.length) {
+        places.forEach((p) => markPlace(p));
+        map.current.on("click", (e) => {
+            const target = e.lngLat;
+            const targetLngLatObj = {latitude: target.lat, longitude: target.lng};
+            const nearby = places.map((p) => {
+                const {longitude, latitude} = p;
+                const pLngLat = {latitude, longitude}
+                const dist = haversine(targetLngLatObj, pLngLat)
+                return dist
+            })
+            console.log(nearby)
+        });
+    }
 
     return (
         <div className="map-wrap">
@@ -51,4 +70,3 @@ export default function Map() {
         </div>
     );
 }
-
