8527596faa1f746789800102ea0846bccd85b972
randomnamejusttogetby <gytis.pranskunas@stud.techin.lt>
2025-03-26 15:13:53 +0200

    packages, configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/react-template/", // Pakeisk pagal savo repo pavadinimą
+  plugins: [react(), tailwindcss()],
+  base: "/react-template-335027/", // Pakeisk pagal savo repo pavadinimą
 });
